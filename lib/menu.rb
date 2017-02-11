0d3a480c42a9ab023820f82f80e10f02ef1fcdd3
Juan Farias <dientuki@gmail.com>
2017-02-11 14:42:42 -0300

    added more and more

--- lib/menu.rb
+++ lib/menu.rb
@@ -3,18 +3,23 @@ def get_menu
 	menus = Array.new
 
 	menus << {:title =>'Quienes somos',
+						:text =>'<span>Quienes</span> <span>somos</span>',
       		  :href => 'quienes-somos'}
 
 	menus << {:title =>'Productos & servicios',
+	:text =>'<span>Productos</span> <span>& servicios</span>',
 		  :href => 'productos-servicios'}
 		  
   menus << {:title =>'Capacidad Operativa',
+  :text =>'<span>Capacidad</span> <span>Operativa</span>',
       :href => 'capacidad-operativa'}
  
   menus << {:title =>'Experiencia',
+  :text =>'Experiencia',
       :href => 'experiencia'}
       
   menus << {:title =>'Contactenos',
+  :text =>'Contactenos',
       :href => 'contactenos'}
 
 	return menus
