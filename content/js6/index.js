b04ae7972bc5494cd0b6b8226231c6d4a7440514
Juan Farias <dientuki@gmail.com>
2017-02-18 03:53:43 -0300

    added form and map

--- content/js6/index.js
+++ content/js6/index.js
@@ -1,11 +1,13 @@
+import SendForm from './modules/SendForm';
 import LazyLoad from 'vanilla-lazyload';
 import Navigation from "./modules/Navigation";
 
 new Navigation();
+new SendForm('#contact-form');
 
 let myLazyLoad = new LazyLoad({
   // example of options object -> see options section
   elements_selector: ".lzl",
   data_src: "original",
-  data_srcset: "original-set",
+  data_srcset: "original-set"
 });
---
+++ content/js6/modules/SendForm.js
@@ -0,0 +1,36 @@
+import Axios from 'axios';
+
+export default class SendForm {
+  constructor(form) {
+    this.form = document.querySelector(form);
+    this.data = [];
+    this.form.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      for (let element of this.form.elements){
+        this.data[element.name] = element.value;
+      }
+
+      this.onSubmit();
+    });
+  }
+
+  onSubmit() {
+
+    let url = 'https://www.google.com/recaptcha/api/siteverify?secret=6LcA_xUUAAAAAFrAa_54CsV4Pu2d7RK6l8yIS2sD&response=' + document.querySelector('#g-recaptcha-response').value
+
+    Axios({ method: 'get',
+            url: url
+    }).then(function (response) {
+        console.log(this.data, response['success']);
+
+        if (response['success'] == true){
+          Axios({ method: 'post',
+            url: 'sendform.php',
+            data: this.data
+          })
+        }
+      });
+  }
+}
+
