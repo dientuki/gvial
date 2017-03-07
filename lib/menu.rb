d972313ac4e61620afbe49d7c99fa92ba65b81ad
Juan Farias <dientuki@gmail.com>
2017-03-07 00:08:53 -0300

    some fixs

--- lib/menu.rb
+++ lib/menu.rb
@@ -25,28 +25,3 @@ def get_menu
 	return menus
 
 end
-
-def get_workUs
-
-	wus = Array.new
-
-	wus << {:title =>'Patagonia Argentina',
-     		  :w => '192'}
-
-	wus << {:title =>'Norte Argentina',
-     		  :w => '113'}
-
-  wus << {:title =>'Cordoba',
-     		  :w => '123'}
-
-	wus << {:title =>'Turismo Buenos Aires',
-     		  :w => '110'}
-
-  wus << {:title =>'Cuyo Argentina',
-     		  :w => '113'}
-
-	wus << {:title =>'Litoral Argentina',
-     		  :w => '137'}
-
-  return wus
-end
